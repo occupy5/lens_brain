812a46dc6ad1e10576fea0c79f61384090c673db
occupy5 <1370275510@qq.com>
2023-09-01 21:40:34 +0800

    add walletprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Logo from "@/components/ui/logo";
+import { Button } from '@/components/ui/button'
 import { ThemeButton } from '@/components/ui/ThemeButton';
 import Providers from "@/components/Providers";
+import { useWeb3Modal } from '@web3modal/react';
+import { useAccount } from 'wagmi';
+import { disconnect } from '@wagmi/core';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Lens Brain',
-  description: 'Build your second brains use lens',
-}
+// export const metadata = {
+//   title: 'Lens Brain',
+//   description: 'Build your second brains use lens',
+// }
 
 export default function RootLayout({
   children,
@@ -20,12 +26,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
+          <Nav></Nav>
           <main className="flex flex-col min-h-screen pt-24 px-4">
-            <header className="flex flex-row justify-center items-center h-24 w-full">
-              <a href='/'>
-                <Logo />
-              </a>
-            </header>
             {children}
             <footer className="flex justify-end p-6">
               <ThemeButton />
@@ -34,5 +36,34 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
+  )
+}
+
+function Nav() {
+  const { open } = useWeb3Modal()
+  const { address } = useAccount()
+
+  return (
+    <nav className='border-b flex flex-row items-center justify-between px-4 py-4'>
+      <div>
+        <Logo />
+      </div>
+      <div>
+        {
+          !address && (
+            <Button onClick={open}>
+              Sign In
+            </Button>
+          )
+        }
+        {
+          address && (
+            <Button onClick={disconnect}>
+              Sign Out
+            </Button>
+          )
+        }
+      </div>
+    </nav>
   )
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import { LensProvider, LensConfig, production } from "@lens-protocol/react-web";
 import { bindings as wagmiBindings } from "@lens-protocol/wagmi";
 import { PublisherContextProvider } from "@/context/ProfileContext";
+import { WalletProvider } from './WalletProvider'
 
 
 const lensConfig: LensConfig = {
@@ -14,7 +15,9 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
       <LensProvider config={lensConfig}>
         <PublisherContextProvider>
-          <ThemeProvider attribute="class" enableSystem={true}>{children}</ThemeProvider>
+          <WalletProvider>
+            <ThemeProvider attribute="class" enableSystem={true}>{children}</ThemeProvider>
+          </WalletProvider>
         </PublisherContextProvider>
       </LensProvider>
   );
